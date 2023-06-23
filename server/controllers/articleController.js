4ddc021d3555a11cb15b83625a4dca9dad407571
dayeon <wjdals3249@naver.com>
2023-06-23 16:04:42 +0900

    23-06-23

--- server/controllers/articleController.js
+++ server/controllers/articleController.js
@@ -8,19 +8,196 @@ const fileHandler = require('../utils/fileHandler')
 exports.feed = async(req, res, next) => {}
 
 // 게시물 가져오기
-exports.find = async(req, res, next) => {}
+exports.find = async (req, res, next) => {
+  try { // #README 1
+    const where = {} // 검색조건
+    const limit = req.query.limit || 9 //한 번에 전송할 도큐먼트의 갯수
+    const skip = req.query.skip || 0 // 건너 뛸 도큐먼트 갯수
 
-// 게시물 한 개 가져오기
-exports.findOne = async(req, res, next) => {}
+    // req.query -> 클라이언트의 요청
+    if ('username' in req.query) { // 타임라인
+      // 클라이언트가 요청한 username으로 유저를 검색한다
+      const user = await User.findOne({ username: req.query.username });
+      // 조건 추가
+      where.author = user._id;
+    }
+
+    // 게시물 검색
+    const articleCount = await Article.count(where); // 해당 도큐먼트의 갯수를 구한다
+    // Document
+    const articles = await Article 
+      .find(where, 'photos favoriteCount created') // find(검색조건, 검색필드)
+      .populate({ // 컬렉션 조인
+        path: 'commentCount' // 게시물의 댓글 갯수 파악
+      })
+      .sort({ created: 'desc' }) // 생성일 기준 내림차순 정렬 (sort)
+      .limit(limit)
+      .skip(skip)
+
+    res.json({ articles, articleCount });
+
+  } catch (error) {
+    next(error)
+  }
+}
+
+// 게시물 상세보기
+exports.findOne = async (req, res, next) => {
+  try {
+    const article = await Article
+      .findById(req.params.id) // 클라이언트가 요청한 id로 게시물을 검색한다
+      .populate({ // 컬렉션 조인
+        path: 'author', // 유저 (게시물 작성자) 정보
+        select: 'username avatar' // 검색 필드
+      })
+      .populate({
+        path: 'isFavorite' // 좋아요 여부
+      })
+      .populate({
+        path: 'commentCount' // 댓글 갯수
+      })
+
+    if (!article) { // 게시물이 없을 경우
+      const err = new Error("Article not found");
+      err.status = 404; // 404 Not found 
+      throw err;
+    }
+    // 검색한 게시물을 전송한다
+    res.json({ article });
+
+  } catch (error) {
+    next(error)
+  }
+}
 
 // 게시물 업로드
-exports.create = []
+exports.create = [
+  // 파일처리 (사진)
+  fileHandler('articles').array('photos'),
+  async (req, res, next) => {
+    try {
+      // req.files: 클라이언트가 전송한 파일
+      const files = req.files;
+
+      // 파일이 없을 경우
+      if (files.length < 1) {
+        const err = new Error('File is required');
+        err.status = 400;
+        throw err;
+      }
+
+      const photos = files.map(file => file.filename);
+
+      // article 도큐먼트를 생성한다
+      const article = new Article({
+        photos,
+        description: req.body.description,
+        author: req.user._id
+      });
+
+      await article.save();
+
+      // 전송
+      res.json({ article });
+
+    } catch (error) {
+      next(error)
+    }
+  }
+]
 
 // 게시물 삭제
-exports.delete = async(req, res, next) => {}
+exports.delete = async(req, res, next) => {
+  try {
+    // 클라이언트가 전송한 id로 게시물을 검색한다
+    const article = await Article.findById(req.params.id)
+
+    // 게시물이 존재하지 않을 경우 
+    if(!article) {
+      const err = new Error("Article not found")
+      err.status = 404 // 404 Notfound
+      throw err;
+    }
+
+    // 본인 게시물이 아닌 경우 삭제 불가
+    // _id : ObjectId -> 객체끼리 비교불가, toString()을 이용해 문자열로 변환하여 비교
+    if (req.user._id.toString() !== article.author.toString()) {
+      const err = new Error ("Author is not correct")
+      err.status = 400
+      throw err
+    }
+
+    await article.deleteOne() // 게시물 삭제 처리
+
+    // 삭제한 게시물 전송
+    res.json({ article })
+
+  } catch (error) {
+    next(error)
+  }
+}
 
 // 좋아요
-exports.favorite = async(req, res, next) => {}
+exports.favorite = async(req, res, next) => {
+  try {
+    // 클라이언트가 전송한 id로 게시물을 전송한다.
+    const article = await Article.findById(req.params.id)
+
+    if (!article) { // 게시물이 없을 경우
+      const err = new Error("Article not found")
+      err.status = 404
+      throw err
+    }
+
+    // 이미 좋아요한 게시물인 경우
+    const _favorite = await Favorite
+      .findOne({user: req.user._id, article: article._id})
+
+    // 처음 좋아요하는 게시물일 경우
+    if(!_favorite) {
+      const favorite = new Favorite({
+        user: req.user._id,
+        article: article._id
+      })
+      await favorite.save()
+
+      // 게시물의 좋아요 갯수를 1 증가시킨다
+      article.favoriteCount++
+      await article.save()
+    }
+
+    res.json({ article })
+  }catch(error) {
+    next(error)
+  }
+}
 
 // 좋아요 취소
-exports.unfavorite = async(req, res, next) => {}
+exports.unfavorite = async(req, res, next) => {
+  try {
+    // 게시물 검색
+    const article = await Article.findById(req.params.id)
+
+    // 게시물이 없는 경우
+    if (!article) {
+      const err = new Error("Article not found")
+      err.status = 404
+      throw err
+    }
+
+    // 좋아요 한 게시물인지 확인
+    const favorite = await Favorite
+      .findOne({ user: req.user._id, article: article._id })
+
+    if (favorite) { // 좋아요 한 게시물이면 좋아요 취소
+      await favorite.deleteOne()
+
+      article.favoriteCount--;
+      await article.save()
+    }
+
+    res.json({ article })
+  } catch (error) {
+    next(error)
+  }
+}
--- server/controllers/userController.js
+++ server/controllers/userController.js
@@ -2,11 +2,171 @@ const User = require("../models/User")
 const fileHandler = require('../utils/fileHandler')
 const {body ,validationResult} = require('express-validator')
 
+const isValidUsername = () => body('username')
+  .trim()
+  .isLength({ min: 5 }).withMessage('Username must be at least 5 characters')
+  .isAlphanumeric().withMessage("Username is only allowed in alphabet and number.")
+
+const isValidEmail = () => body('email')
+  .trim()
+  .isEmail().withMessage('E-mail is not valid')
+
+const isValidPassword = () => body('password')
+  .trim()
+  .isLength({ min: 5 }).withMessage('Password must be at least 5 characters')
+
+const emailInUse = async (email) => {
+  const user = await User.findOne({ email });
+  if (user) {
+    return Promise.reject('E-mail is already in use');
+  }
+}
+
+const usernameInUse = async (username) => {
+  const user = await User.findOne({ username });
+  if (user) {
+    return Promise.reject('Username is already in use');
+  }
+}
+
+const doesEmailExists = async (email) => {
+  const user = await User.findOne({ email });
+  if (!user) {
+    return Promise.reject('User is not found');
+  }
+}
+
+const doesPasswordMatch = async (password, { req }) => {
+  const email = req.body.email;
+  const user = await User.findOne({ email });
+  if (!user.checkPassword(password)) {
+    return Promise.reject('Password does not match');
+  }
+}
+
 // 회원가입
-exports.create = []
+exports.create = [
+  isValidUsername().custom(usernameInUse),
+  isValidEmail().custom(emailInUse),
+  isValidPassword(),
+  async (req, res, next) => {
+    try {
+
+      const errors = validationResult(req);
+
+      if (!errors.isEmpty()) {
+        const err = new Error();
+        err.errors = errors.array();
+        err.status = 400;
+        throw err;
+      }
+
+      const { email, fullName, username, password } = req.body;
+
+      const user = new User();
+
+      user.email = email;
+      user.fullName = fullName;
+      user.username = username;
+      user.setPassword(password);
+
+      await user.save();
+
+      res.json({ user });
+
+    } catch (error) {
+      next(error)
+    }
+  }
+]
 
 // 정보 수정
-exports.update = []
+exports.update = [
+  // 프로필 사진(파일) 처리
+   fileHandler('profiles').single('avatar'),
+   // 유효성 검사
+   // 입력데이터가 있을 경우 유효성 검사
+  isValidUsername().custom(usernameInUse).optional(),
+  isValidEmail().custom(emailInUse).optional(),
+  async(req, res, next) => {
+    try { // 유효성 검사 결과
+      const errors = validationResult(req);
+
+      // 검사 실패
+      if (!errors.isEmpty()) {
+        const err = new Error();
+        err.errors = errors.array();
+        err.status = 400; // 400 Bad Request
+        throw err
+      }
+
+      // req.user: 로그인 유저
+      const _user = req.user;
+
+      // 프로필 사진을 업로드한 경우
+      if (req.file) { // req.file : 클라이언트가 전송한 파일
+        _user.avatar = req.file.filename;
+      }
+      // 로그인 유저의 정보 중 클라이언트가 수정 요청을 한 정보만 업데이트한다
+      Object.assign(_user, req.body);
+
+      await _user.save() // 변경사항을 저장한다
+
+      // 토큰을 재발급한다
+      const token = _user.generateJWT()
+
+      const user = {
+        username: _user.username,
+        email: _user.emil,
+        fullName: _user.fullName,
+        avatar: _user.avatar,
+        bio: _user.bio,
+        token
+      }
+      // 업데이트한 유저와 토큰을 전송한다
+      res.json({user})
+
+    } catch (error) {
+      next(error)
+    }
+  }
+]
 
 // 로그인
-exports.login =[]
+exports.login = [
+  isValidEmail().custom(doesEmailExists),
+  isValidPassword().custom(doesPasswordMatch),
+  async (req, res, next) => {
+    try {
+
+      const errors = validationResult(req);
+
+      if (!errors.isEmpty()) {
+        const err = new Error();
+        err.errors = errors.array();
+        err.status = 401;
+        throw err;
+      }
+
+      const { email } = req.body;
+
+      const _user = await User.findOne({ email });
+
+      const token = _user.generateJWT();
+  
+      const user = {
+        username: _user.username,
+        email: _user.email,
+        fullName: _user.fullName,
+        avatar: _user.avatar,
+        bio: _user.bio,
+        token
+      }
+  
+      res.json({ user })
+  
+    } catch (error) {
+      next(error)
+    }
+  }
+]
